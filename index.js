c7a915416fc7b3f1214d90f17a675fff5a47fe45
rohit8020 <rohitgangwar8020@gmail.com>
2021-09-03 20:54:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+let input=document.getElementById("input")
+let num_c_point=document.querySelectorAll(".item")
+let operators=document.querySelectorAll(".operator")
+let equal=document.getElementById("equal")
+let expression=""
+
+input.addEventListener("change",()=>{
+    expression=input.value
+})
+
+equal.addEventListener("click", ()=>{
+    let answer
+    try {
+        let temp_ans=""
+        let i=0
+        for(i in input.value){
+            if(input.value[i]=="X"){
+                temp_ans+="*"
+            }else if(input.value[i]=="÷"){
+                temp_ans+="/"
+            }else{
+                temp_ans+=input.value[i]
+            }
+        }
+        answer=eval(temp_ans)
+    } catch (error) {
+        answer="INVALID"
+    }
+    input.value=answer
+    expression=""
+})
+
+operators.forEach(operator=>{
+    operator.addEventListener("click", ()=>{
+        if(expression.length==0){
+            expression=input.value+operator.innerHTML
+            input.value=expression
+        }else{
+            expression=expression+operator.innerHTML
+            input.value=expression
+        }
+    })
+})
+
+num_c_point.forEach(btn=>{
+    btn.addEventListener("click", ()=>{
+        if(btn.innerHTML==="C"){
+            expression=""
+            input.value=""
+        }else{
+            expression=expression+btn.innerHTML
+            input.value=expression
+        }
+    })
+})
+
+
+
